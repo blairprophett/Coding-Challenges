7f64fe76864f89b12dae56030cecd20a3f08504b
blairprophett <blaircuny@gmail.com>
2014-06-13 11:02:45 -0700

    first commit, starting open response problems

---
+++ open_response.rb
@@ -0,0 +1,27 @@
+Open Response Problems:
+
+  1. Explain how HTTP works in detail. Use diagrams.
+
+
+
+
+  2. Explain how APIs work. In detail. Diagrams would be helpful.
+
+
+
+
+  3. Explain how method_missing works. Show an example.
+
+
+
+
+  4. What is the purpose of yield? Show an example.
+
+
+
+
+  5. Explain how MVC works. Why is it beneficial? Diagrams, please.
+
+
+
+  
